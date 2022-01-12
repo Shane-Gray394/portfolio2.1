221de23e649fd5218d883ebcaee3db780658ed81
Shane-Gray394 <86175936+Shane-Gray394@users.noreply.github.com>
2022-01-12 07:03:51 -0500

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '../globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
